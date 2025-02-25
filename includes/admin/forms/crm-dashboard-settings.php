ba34cf429457d9da60730e78c1099cb4ff74f823
Rolly Bueno <rollybueno@gmail.com>
2025-02-25 19:30:22 +0800

    Fix: Dashboard Settings

--- includes/admin/forms/crm-dashboard-settings.php
+++ includes/admin/forms/crm-dashboard-settings.php
@@ -38,7 +38,7 @@ class Dx_Crm_Dashboard_Settings {
 		<!-- Start wrap div -->
 		<div class="wrap">
 			<div>
-				<h2><?php _e( 'DX Sales CRM Dashboard', 'dxcrm' ); ?></h2>
+				<h2><?php esc_html_e( 'DX Sales CRM Dashboard', 'dxcrm' ); ?></h2>
 			</div>
 			
 			<!-- Start dashboard widget wrap div -->
@@ -52,8 +52,8 @@ class Dx_Crm_Dashboard_Settings {
 							
 							<!-- Start dashboard right now div -->
 							<div class="postbox " id="dashboard_right_now">								
-								<div title="Click to toggle" class="handlediv"><br></div>
-								<h3 class="hndle"><span><?php _e( 'Companies', 'dxcrm' ); ?></span></h3>
+								<div title="<?php esc_attr_e('Click to toggle', 'dxcrm'); ?>" class="handlediv"><br></div>
+								<h3 class="hndle"><span><?php esc_html_e( 'Companies', 'dxcrm' ); ?></span></h3>
 								
 								<!-- Start inside div -->
 								<div class="inside">									
@@ -87,23 +87,23 @@ class Dx_Crm_Dashboard_Settings {
 															 	if(isset($company_data->publish) & !empty($company_data->publish)) { $company_count = $company_data->publish; } else { $company_count = '0'; }
 														 	}
 
-														 	echo $company_count;
+														 	echo esc_html($company_count);
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Total Companies', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Total Companies', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">
 													<?php if( current_user_can('administrator') ){ ?>
-											            <a href="post-new.php?post_type=<?php echo DX_CRM_POST_TYPE_COMPANY; ?>" class="dx-crm-box-bottom">
-															<label class="dx-crm-add-new"><?php _e( 'Add New', 'dxcrm' ); ?></label>
+											            <a href="<?php echo esc_url(admin_url('post-new.php?post_type=' . DX_CRM_POST_TYPE_COMPANY)); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-add-new"><?php esc_html_e( 'Add New', 'dxcrm' ); ?></label>
 														</a>
 														
-														<a href="javascript:void(0);" title="Add New Company" class="dx-crm-box-bottom dx-crm-add-company-link">
-											            	<label class="dx-crm-quick-add"><?php _e( 'Quick Add', 'dxcrm' ); ?></label>
+														<a href="javascript:void(0);" title="<?php esc_attr_e('Add New Company', 'dxcrm'); ?>" class="dx-crm-box-bottom dx-crm-add-company-link">
+											            	<label class="dx-crm-quick-add"><?php esc_html_e( 'Quick Add', 'dxcrm' ); ?></label>
 											            </a>
 													<?php } ?> 
-														<a href="edit.php?post_type=<?php echo DX_CRM_POST_TYPE_COMPANY; ?>" title="View All Companies" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="<?php echo esc_url(admin_url('edit.php?post_type=' . DX_CRM_POST_TYPE_COMPANY)); ?>" title="<?php esc_attr_e('View All Companies', 'dxcrm'); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>													
 													</div>
 												</div>
@@ -124,14 +124,14 @@ class Dx_Crm_Dashboard_Settings {
 														<div class="number">															
 														 <?php
 														 	$ce_data = wp_count_posts(DX_CRM_POST_TYPE_COMPANY_EXPENSES);														
-														 	if(isset($ce_data->publish) & !empty($ce_data->publish)) { echo $ce_data->publish; } else { echo '0'; }
+														 	if(isset($ce_data->publish) & !empty($ce_data->publish)) { echo esc_html($ce_data->publish); } else { echo '0'; }
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Total Company Expenses', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Total Company Expenses', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">														
-														<a href="edit.php?post_type=crm_company_expenses" title="View All Company Expenses" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="<?php echo esc_url(admin_url('edit.php?post_type=crm_company_expenses')); ?>" title="<?php esc_attr_e('View All Company Expenses', 'dxcrm'); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													</div>
 												</div>
@@ -150,7 +150,7 @@ class Dx_Crm_Dashboard_Settings {
 							<!-- Start dashboard activity div -->
 							<div class="postbox " id="dashboard_activity">
 								<div title="Click to toggle" class="handlediv"><br></div>
-								<h3 class="hndle"><span><?php _e( 'Sales CRM', 'dxcrm' ); ?></span></h3>
+								<h3 class="hndle"><span><?php esc_html_e( 'Sales CRM', 'dxcrm' ); ?></span></h3>
 								<!-- Start inside div -->
 								<div class="inside">
 									<div class="main">
@@ -162,17 +162,19 @@ class Dx_Crm_Dashboard_Settings {
 												<div class="dx-crm-dashboard-stat dx-crm-dashboard-stat-light green-soft">
 													<div class="visual"><i class="fa fa-history"></i></div>
 													<div class="details">
-														<div class="number">															
+														<div class="number">														
 														 <?php
+														 	// Ignoring this line as it's already escaped
+															// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 														 	echo $this->activity_log->count_all_roadmaps();
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Activity Log', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Activity Log', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">	
 													<?php if( current_user_can('administrator') ){ ?>
-														<a href="admin.php?page=dx-crm-activity-log" title="<?php _e( 'View All Logs', 'dxcrm' ); ?>" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="admin.php?page=dx-crm-activity-log" title="<?php esc_attr_e( 'View All Logs', 'dxcrm' ); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													<?php } ?>
 													</div>
@@ -182,11 +184,11 @@ class Dx_Crm_Dashboard_Settings {
 											<div class="col-lg-6 col-md-12">												
 												<div class="dx-crm-dashboard-stat dx-crm-dashboard-stat-light pink-soft">	
 													<div class="visual"><i class="fa fa-bar-chart-o"></i></div>
-													<div class="details"><div class="number"><?php _e( 'Reports', 'dxcrm' ); ?></div></div>
+													<div class="details"><div class="number"><?php esc_html_e( 'Reports', 'dxcrm' ); ?></div></div>
 													<div class="more">
 														<a class="dx-crm-box-bottom"><label></label></a>
 														<a href="admin.php?page=dx-crm-stat-setting" title="View All Reports" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													</div>
 												</div>
@@ -209,7 +211,7 @@ class Dx_Crm_Dashboard_Settings {
 							<!-- Start dashboard primary div -->
 							<div class="postbox" id="dashboard_primary">
 								<div title="Click to toggle" class="handlediv"><br></div>
-								<h3 class="hndle"><span><?php _e( 'Customers', 'dxcrm' ); ?></span></h3>
+								<h3 class="hndle"><span><?php esc_html_e( 'Customers', 'dxcrm' ); ?></span></h3>
 								<!-- Start inside div -->
 								<div class="inside">
 									<div class="main">
@@ -223,22 +225,22 @@ class Dx_Crm_Dashboard_Settings {
 														<div class="number">															
 														 <?php
 														 	$company_data = wp_count_posts(DX_CRM_POST_TYPE_CUSTOMERS);	
-														 	if(isset($company_data->publish) & !empty($company_data->publish)) { echo $company_data->publish; } else { echo '0'; }
+														 	if(isset($company_data->publish) & !empty($company_data->publish)) { echo esc_html($company_data->publish); } else { echo '0'; }
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Total Customers', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Total Customers', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">
 													<?php if( current_user_can('administrator') ){ ?>
-														<a href="post-new.php?post_type=<?php echo DX_CRM_POST_TYPE_CUSTOMERS; ?>" class="dx-crm-box-bottom">
-															<label class="dx-crm-add-new"><?php _e( 'Add New', 'dxcrm' ); ?></label>
+														<a href="post-new.php?post_type=<?php echo esc_url(DX_CRM_POST_TYPE_CUSTOMERS); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-add-new"><?php esc_html_e( 'Add New', 'dxcrm' ); ?></label>
 														</a>
 														
-														<a href="javascript:void(0);" title="Add New Customer" class="dx-crm-box-bottom dx-crm-add-customer-link">
-											            	<label class="dx-crm-quick-add"><?php _e( 'Quick Add', 'dxcrm' ); ?></label>
+														<a href="javascript:void(0);" title="<?php esc_attr_e('Add New Customer', 'dxcrm'); ?>" class="dx-crm-box-bottom dx-crm-add-customer-link">
+											            	<label class="dx-crm-quick-add"><?php esc_html_e( 'Quick Add', 'dxcrm' ); ?></label>
 											            </a>
-														<a href="edit.php?post_type=<?php echo DX_CRM_POST_TYPE_CUSTOMERS; ?>" title="View All Customers" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="edit.php?post_type=<?php echo esc_url(DX_CRM_POST_TYPE_CUSTOMERS); ?>" title="<?php esc_attr_e('View All Customers', 'dxcrm'); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													<?php } ?>
 													</div>
@@ -251,15 +253,15 @@ class Dx_Crm_Dashboard_Settings {
 														<div class="number">															
 														 <?php
 														 	$company_data = wp_count_terms(DX_CRM_SKILL_TAXONOMY);														 	
-														 	if(isset($company_data) & !empty($company_data)) { echo $company_data; } else { echo '0'; }
+														 	if(isset($company_data) & !empty($company_data)) { echo esc_html($company_data); } else { echo '0'; }
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Total Skills', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Total Skills', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">
 													<?php if( current_user_can('administrator') ){ ?>
-														<a href="edit-tags.php?taxonomy=<?php echo DX_CRM_SKILL_TAXONOMY; ?>&post_type=<?php echo DX_CRM_POST_TYPE_CUSTOMERS; ?>" title="View All Skills" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="edit-tags.php?taxonomy=<?php echo esc_attr(DX_CRM_SKILL_TAXONOMY); ?>&post_type=<?php echo esc_attr(DX_CRM_POST_TYPE_CUSTOMERS); ?>" title="<?php esc_attr_e('View All Skills', 'dxcrm'); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													<?php } ?>
 													</div>
@@ -276,7 +278,7 @@ class Dx_Crm_Dashboard_Settings {
 							<!-- Start dashboard quick press div -->
 							<div class="postbox" id="dashboard_quick_press">
 								<div title="Click to toggle" class="handlediv"><br></div>
-								<h3 class="hndle"><span><?php _e( 'Projects', 'dxcrm' ); ?></span></h3>
+								<h3 class="hndle"><span><?php esc_html_e( 'Projects', 'dxcrm' ); ?></span></h3>
 								<!-- Start inside div -->
 								<div class="inside">
 									<div class="main">
@@ -308,22 +310,22 @@ class Dx_Crm_Dashboard_Settings {
 															 	if(isset($project_data->publish) & !empty($project_data->publish)) { $project_count = $project_data->publish; } else { $project_count = '0'; }
 														 	}
 
-														 	echo $project_count
+														 	echo esc_html($project_count);
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Total Projects', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Total Projects', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">
 													<?php if( current_user_can('administrator') ){ ?>
-														<a href="post-new.php?post_type=<?php echo DX_CRM_POST_TYPE_PROJECTS; ?>" class="dx-crm-box-bottom">
-															<label class="dx-crm-add-new"><?php _e( 'Add New', 'dxcrm' ); ?></label>
+														<a href="post-new.php?post_type=<?php echo esc_url(DX_CRM_POST_TYPE_PROJECTS); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-add-new"><?php esc_html_e( 'Add New', 'dxcrm' ); ?></label>
 														</a>
-														<a href="javascript:void(0);" title="Add New Project" class="dx-crm-box-bottom dx-crm-add-project-link">
-											            	<label class="dx-crm-quick-add"><?php _e( 'Quick Add', 'dxcrm' ); ?></label>
+														<a href="javascript:void(0);" title="<?php esc_attr_e('Add New Project', 'dxcrm'); ?>" class="dx-crm-box-bottom dx-crm-add-project-link">
+											            	<label class="dx-crm-quick-add"><?php esc_html_e( 'Quick Add', 'dxcrm' ); ?></label>
 											            </a>
 													<?php } ?>
-														<a href="edit.php?post_type=<?php echo DX_CRM_POST_TYPE_PROJECTS; ?>" title="View All Projects" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="edit.php?post_type=<?php echo esc_url(DX_CRM_POST_TYPE_PROJECTS); ?>" title="<?php esc_attr_e('View All Projects', 'dxcrm'); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													</div>													
 												</div>
@@ -335,15 +337,15 @@ class Dx_Crm_Dashboard_Settings {
 														<div class="number">
 														 <?php
 														 	$company_data = wp_count_terms(DX_CRM_PRO_TAXONOMY);
-														 	if(isset($company_data) & !empty($company_data)) { echo $company_data; } else { echo '0'; }
+														 	if(isset($company_data) & !empty($company_data)) { echo esc_html($company_data); } else { echo '0'; }
 														 ?>
 														</div>
-														<div class="desc"><?php _e( 'Total Project Types', 'dxcrm' ); ?></div>
+														<div class="desc"><?php esc_html_e( 'Total Project Types', 'dxcrm' ); ?></div>
 													</div>
 													<div class="more">	
 													<?php if( current_user_can('administrator') ){ ?>
-														<a href="edit-tags.php?taxonomy=<?php echo DX_CRM_PRO_TAXONOMY; ?>&post_type=<?php echo DX_CRM_POST_TYPE_PROJECTS; ?>" title="View All Project Types" class="dx-crm-box-bottom">
-															<label class="dx-crm-view-more"><?php _e( 'View more', 'dxcrm' ); ?></label>
+														<a href="edit-tags.php?taxonomy=<?php echo esc_attr(DX_CRM_PRO_TAXONOMY); ?>&post_type=<?php echo esc_attr(DX_CRM_POST_TYPE_PROJECTS); ?>" title="<?php esc_attr_e('View All Project Types', 'dxcrm'); ?>" class="dx-crm-box-bottom">
+															<label class="dx-crm-view-more"><?php esc_html_e( 'View more', 'dxcrm' ); ?></label>
 														</a>
 													<?php } ?>
 													</div>
