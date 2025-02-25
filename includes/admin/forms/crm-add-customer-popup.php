ae584dcbedbb7b352d3e43f455d5d69b299f3929
Rolly Bueno <rollybueno@gmail.com>
2025-02-25 20:28:59 +0800

    Fix: Escaping Add Customer Popup

--- includes/admin/forms/crm-add-customer-popup.php
+++ includes/admin/forms/crm-add-customer-popup.php
@@ -17,26 +17,26 @@ if ( !defined( 'ABSPATH' ) ) exit;
 <div class="dx-crm-cust-popup-content">
 
 	<div class="dx-crm-cust-header">
-		<div class="dx-crm-cust-header-title"><?php _e( 'Add New Customer', 'dxcrm' );?></div>
-		<div class="dx-crm-cust-popup-close"><a href="javascript:void(0);" class="dx-crm-cust-close-button"><img src="<?php echo DX_CRM_IMG_URL;?>/tb-close.png" title="Close"></a></div>
+		<div class="dx-crm-cust-header-title"><?php esc_html_e( 'Add New Customer', 'dxcrm' );?></div>
+		<div class="dx-crm-cust-popup-close"><a href="javascript:void(0);" class="dx-crm-cust-close-button"><img src="<?php echo esc_url( DX_CRM_IMG_URL );?>/tb-close.png" title="<?php esc_html_e( 'Close', 'dxcrm' );?>"></a></div>
 	</div>
 	
 	<div class="dx-crm-cust-popup">
 		<div class="dx-crm-cust-add-customer-title">
 			<div>
-				<label for="dx_crm_customer_title"><?php _e( 'Enter Customer Title', 'dxcrm' );?></label>
+				<label for="dx_crm_customer_title"><?php esc_html_e( 'Enter Customer Title', 'dxcrm' );?></label>
 				<input type="text" class="dx_crm_customer_title_text" name="dx_crm_customer_title" id="dx_crm_customer_title" size="31">
 			</div>
-			<div class="dx-crm-cust-title-error"><?php _e('Please Enter title for customer', 'dxcrm') ?></div>
+			<div class="dx-crm-cust-title-error"><?php esc_html_e('Please Enter title for customer', 'dxcrm') ?></div>
 			<br /><div>
-				<label for="dx_crm_customer_email"><?php _e( 'Enter Customer Email', 'dxcrm' );?></label>
+				<label for="dx_crm_customer_email"><?php esc_html_e( 'Enter Customer Email', 'dxcrm' );?></label>
 				<input type="text" class="dx_crm_customer_email_text" name="dx_crm_customer_email" id="dx_crm_customer_email" size="31">
 			</div>			
-			<div class="dx-crm-cust-email-error"><?php _e('Please Enter email for customer', 'dxcrm') ?></div>
-			<div class="dx-crm-cust-title-success"><?php _e('Customer Added Successfully.', 'dxcrm') ?></div>
+			<div class="dx-crm-cust-email-error"><?php esc_html_e('Please Enter email for customer', 'dxcrm') ?></div>
+			<div class="dx-crm-cust-title-success"><?php esc_html_e('Customer Added Successfully.', 'dxcrm') ?></div>
 			<div>
-				<input type="button" class="dx_crm_add_customer_button button" id="dx_crm_add_customer_button" value="Add customer" />
-				<input type="button" class="dx_crm_add_more_customer_button button" id="dx_crm_add_more_customer_button" value="Save and Add More" />			
+				<input type="button" class="dx_crm_add_customer_button button" id="dx_crm_add_customer_button" value="<?php esc_html_e( 'Add customer', 'dxcrm' ); ?>" />
+				<input type="button" class="dx_crm_add_more_customer_button button" id="dx_crm_add_more_customer_button" value="<?php esc_html_e( 'Save and Add More', 'dxcrm' ); ?>" />			
 			</div>			
 		</div>		
 	</div><!--.edd-points-popup-->
